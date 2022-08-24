a9ea242b074f0704aed2710bd4aa4609fbbb6690
Philipp Thiele <philipp.thiele@phth.de>
2022-08-24 12:26:20 +0200

    [TASK] raise dependency for typo3 11 LTS

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = array(
     'version' => '1.0.0',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '10.4.0-10.4.99'
+            'typo3' => '11.5.12-11.5.99'
         ),
         'conflicts' => array(),
         'suggests' => array(),
